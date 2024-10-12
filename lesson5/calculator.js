c9cb044bec79d8d0dee2e3722b83c7b1ef79c179
Sofia-Umanets <sonatennis2006@gmail.com>
2024-10-12 12:08:00 +0300

    Add files via upload

---
+++ lesson5/calculator.js
@@ -0,0 +1,14 @@
+document.getElementById('calculate').addEventListener('click', function() {
+
+    var productPrice = parseFloat(document.getElementById('product').value);
+
+    var count = parseInt(document.getElementById('quantity').value);
+
+    if (isNaN(count) || count <= 0) {
+        document.getElementById('result').textContent = 'Введите целое число больше нуля для количества товара!';
+        return; 
+    }
+    var totalCost = productPrice * count;
+
+    document.getElementById('result').textContent = 'Стоимость заказа ' + totalCost + ' монет.';
+});
