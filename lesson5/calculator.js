dc0eb8308330cffec08c9fbfe7177dae2c5ccb7f
Sofia-Umanets <sonatennis2006@gmail.com>
2024-10-12 13:01:36 +0300

    Update calculator.js

--- lesson5/calculator.js
+++ lesson5/calculator.js
@@ -2,7 +2,7 @@ document.getElementById('calculate').addEventListener('click', function() {
 
     let productPrice = parseFloat(document.getElementById('product').value);
 
-    let count = parseInt(document.getElementById('quantity').value);
+    let count = parseInt(document.getElementById('quantity').value,10);
 
     if (typeof count!=='number' || Number.isNaN(count) || count <= 0 || !Number.isInteger(count)) {
         document.getElementById('result').textContent = 'Введите целое число больше нуля для количества товара!';
