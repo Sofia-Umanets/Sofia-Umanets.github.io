94353548b36d72233a0819ad080398c209d65e95
Sofia-Umanets <sonatennis2006@gmail.com>
2024-10-12 12:37:36 +0300

    Update calculator.js

--- lesson5/calculator.js
+++ lesson5/calculator.js
@@ -1,14 +1,14 @@
 document.getElementById('calculate').addEventListener('click', function() {
 
-    var productPrice = parseFloat(document.getElementById('product').value);
+    let productPrice = parseFloat(document.getElementById('product').value);
 
-    var count = parseInt(document.getElementById('quantity').value);
+    let count = parseInt(document.getElementById('quantity').value);
 
     if (typeof count!=='number' || Number.isNaN(count) || count <= 0) {
         document.getElementById('result').textContent = 'Введите целое число больше нуля для количества товара!';
         return; 
     }
-    var totalCost = productPrice * count;
+    let totalCost = productPrice * count;
 
     document.getElementById('result').textContent = 'Стоимость заказа ' + totalCost + ' монет.';
 });
